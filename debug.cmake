a7a87a8adc545be8126415f2bb5d3b60d6b6df37
SirTobi <code.databyte@gmail.com>
2014-11-16 17:35:52 +0100

    fixed debug beahavior

--- debug.cmake
+++ debug.cmake
@@ -13,13 +13,15 @@ endmacro(buildx_enable_debug)
 function(buildx_debug _msg)
 
 	if(DEFINED BUILDX_DEBUG_CATS)
-		list(FIND BUILDX_DEBUG_CATS "all" CAT_FOUND)
-		
-		# check if cat should be printed
-		if(CAT_FOUND EQUAL "-1" AND DEFINED ARGV1)
-			list(FIND BUILDX_DEBUG_CATS ${ARGV1} CAT_FOUND)
+	
+		if(DEFINED ARGV1)
+			list(FIND BUILDX_DEBUG_CATS "all" CAT_FOUND)
+			
+			# check if cat should be printed
+			if(CAT_FOUND EQUAL "-1")
+				list(FIND BUILDX_DEBUG_CATS ${ARGV1} CAT_FOUND)
+			endif()
 		endif()
-		
 	
 		if(NOT CAT_FOUND EQUAL "-1")
 		
