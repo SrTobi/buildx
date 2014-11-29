ce457dc39e62088c91c402693a7a57d02fb5bc90
SirTobi <code.databyte@gmail.com>
2014-11-29 19:55:17 +0100

    impoved test generator

--- test.cmake
+++ test.cmake
@@ -2,22 +2,82 @@
 set(_BUILDX_TESTX_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/testx/include" PARENT_SCOPE)
 set(_BUILDX_TESTX_MODULE_CONFIG_FILE "${CMAKE_CURRENT_LIST_DIR}/configs/test-module-config.cpp" PARENT_SCOPE)
 
-# buildx_add_test(	target_name
-#					test_path)
-macro(buildx_add_test _name _test_path)
 
-	find_package(Boost REQUIRED)
+# buildx_add_external_test(	test_target
+#							test_path
+#							TEST_TARGETS targets
+#							)
+macro(buildx_add_external_test _target_name _test_path)
+
+	set(options)
+	set(oneValueArgs)
+	set(multiValueArgs TEST_TARGETS)
+	cmake_parse_arguments(_arg "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 
 	# configure module file
-	set(TESTX_MODULE_NAME ${_name})
-	set(config_file_target ${CMAKE_CURRENT_BINARY_DIR}/${_name}-module.cpp)
+	set(TESTX_MODULE_NAME ${_target_name})
+	set(config_file_target ${CMAKE_CURRENT_BINARY_DIR}/${_target_name}-module.cpp)
 	configure_file(${_BUILDX_TESTX_MODULE_CONFIG_FILE} ${config_file_target} @ONLY)
 	
+	if(WIN32)
+		set(Boost_USE_STATIC_LIBS true)
+	endif(WIN32)
+	find_package(Boost COMPONENTS unit_test_framework REQUIRED)
 	
 	buildx_scan(_BUILDX_TMP_TEST_SOURCE ${_test_path} "hpp;cpp")
-	add_executable(${_name} ${ARGN} ${_BUILDX_TMP_TEST_SOURCE} ${config_file_target})
-	set_target_properties(${_name} PROPERTIES COMPILE_DEFINITIONS "TESTX_TEST")
-	target_include_directories(${_name} PRIVATE ${_BUILDX_TESTX_INCLUDE_DIR} ${Boost_INCLUDE_DIRS})
-	target_link_libraries(${_name} ${Boost_LIBRARIES})
+	add_executable(${_target_name} ${_BUILDX_TMP_TEST_SOURCE} ${config_file_target})
+	set_target_properties(${_target_name} PROPERTIES COMPILE_DEFINITIONS "TESTX_TEST")
+	target_link_libraries(${_target_name} ${_arg_TEST_TARGETS} ${Boost_LIBRARIES})
+	target_include_directories(${_target_name} PRIVATE ${_BUILDX_TESTX_INCLUDE_DIR} ${Boost_INCLUDE_DIRS})
 
-endmacro(buildx_add_test)
+endmacro(buildx_add_external_test)
+
+
+# buildx_add_internal_test(	test_target
+#							test_path
+#							TEST_TARGET targets
+#							)
+macro(buildx_add_internal_test _target_name _test_path)
+
+	set(options)
+	set(oneValueArgs TEST_TARGET)
+	set(multiValueArgs)
+	cmake_parse_arguments(_arg "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+	if(NOT _arg_TEST_TARGET)
+		message(FATAL_ERROR "TEST_TARGET not specified!")
+	endif(NOT _arg_TEST_TARGET)
+	
+	
+	# configure module file
+	set(TESTX_MODULE_NAME ${_target_name})
+	set(config_file_target ${CMAKE_CURRENT_BINARY_DIR}/${_target_name}-module.cpp)
+	configure_file(${_BUILDX_TESTX_MODULE_CONFIG_FILE} ${config_file_target} @ONLY)
+	
+	if(WIN32)
+		set(Boost_USE_STATIC_LIBS true)
+	endif(WIN32)
+	find_package(Boost COMPONENTS unit_test_framework REQUIRED)
+	
+	# get target source
+	get_target_property(_org_source ${_arg_TEST_TARGET} SOURCES)
+	
+	# search test source and build executable
+	buildx_scan(_BUILDX_TMP_TEST_SOURCE ${_test_path} "hpp;cpp")
+	add_executable(${_target_name} ${_BUILDX_TMP_TEST_SOURCE} ${config_file_target} ${_org_source})
+	
+	# get definitions
+	get_target_property(_org_definitions ${_arg_TEST_TARGET} COMPILE_DEFINITIONS)
+	if(_org_definitions)
+		set_property(TARGET ${_target_name} PROPERTY  ${_org_definitions})
+	endif()
+	set_property(TARGET ${_target_name} APPEND_STRING PROPERTY COMPILE_DEFINITIONS "TESTX_TEST")
+	set_property(TARGET ${_target_name} APPEND_STRING PROPERTY COMPILE_DEFINITIONS "${_arg_TEST_TARGET}_EXPORTS")
+	
+	# get include dirs
+	get_target_property(_org_include_dirs ${_arg_TEST_TARGET} INCLUDE_DIRECTORIES)
+	
+	target_link_libraries(${_target_name} ${Boost_LIBRARIES})
+	target_include_directories(${_target_name} PRIVATE ${_BUILDX_TESTX_INCLUDE_DIR} ${Boost_INCLUDE_DIRS} ${_org_include_dirs})
+
+endmacro(buildx_add_internal_test)
