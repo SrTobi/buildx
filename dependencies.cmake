f2c44ac8b86b9d151f090ec79c441c7255fd625c
SirTobi <code.databyte@gmail.com>
2014-11-29 21:50:22 +0100

    fixed dependency copy

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,6 +1,6 @@
 
 
-function(buildx_copy_dependency _target)
+function(buildx_copy_dependency _target _to)
 
 	buildx_debug("Examining target '${_target}'" dep)
 	if(NOT TARGET ${_target})
@@ -8,11 +8,13 @@ function(buildx_copy_dependency _target)
 	endif()
 
 	get_property(type TARGET ${_target} PROPERTY TYPE)
-	if(NOT ${type} STREQUAL "STATIC_LIBRARY")
-		buildx_debug("Copy dynamic libraries for target '${_target}'" dep)
-		add_custom_command(	TARGET ctest POST_BUILD
+	get_property(imported TARGET ${_target} PROPERTY IMPORTED)
+	if(NOT ${type} STREQUAL "STATIC_LIBRARY" AND ${imported})
+		buildx_debug("Copy dynamic libraries for target '${_target}' to '${_to}'" dep)
+		add_custom_command(	TARGET ${_to} POST_BUILD
 							COMMAND ${CMAKE_COMMAND} -E
-							copy_if_different $<TARGET_PROPERTY:${_target},IMPORTED_LOCATION_$<UPPER_CASE:$<CONFIG>>> $<TARGET_FILE_DIR:ctest>)
+							copy_if_different $<TARGET_PROPERTY:${_target},IMPORTED_LOCATION_$<UPPER_CASE:$<CONFIG>>> $<TARGET_FILE_DIR:${_to}>)
+		buildx_print_target_properties(${_target})
 	endif()
 
 	# recursive
@@ -28,6 +30,6 @@ function(buildx_copy_target_dependencies _target)
 	get_property(linkl TARGET ${_target} PROPERTY LINK_LIBRARIES)
 	foreach(li ${linkl})
 		
-		buildx_copy_dependency(${li})
+		buildx_copy_dependency("${li}" "${_target}")
 	endforeach()
 endfunction()
